58dc89bdc120fb26e8945c34c64b5f5e31656a4a
Vignesh <vigneshreddy4444@gmail.com>
2024-01-29 14:15:45 +0530

    First Commit

---
+++ Routes/Message-Routes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const { check } = require("express-validator");
+const router = express.Router();
+const messageController = require("../Controllers/Message-Controllers");
+const fileUpload = require("../middleware/file-upload");
+
+router.get("/get/allmessages", messageController.getAllMessages);
+
+router.post(
+  "/create/message",
+  fileUpload.single("image"),
+  [check("contacts").notEmpty()],
+  messageController.createMessage
+);
+router.delete("/delete/message", messageController.deleteMessage);
+module.exports = router;
